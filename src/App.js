11b5dc4ed2099054aeb90709ef9fe236d0cefc41
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-09-16 21:42:53 +0100

    my best

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid, Alert } from "@mui/material";
+import { useQuery } from "react-query";
+import { useLocation } from "react-router";
+import { getAssets } from "./actions";
+import AssetCard from "./components/AssetCard";
+import Loader from "./components/Loader";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const qs = new URLSearchParams(useLocation().search).toString().split("&").map(pair => pair.split("="));
+  // qs to object
+  const params = qs.reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
+  const { data, isLoading, isError } = useQuery(
+    ["all-cases", qs],
+    async () => await getAssets(params)
+  );
+  const assets = data?.assets ?? [];
+  console.log(data, qs);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Container style={{ paddingTop: "1.5rem" }}>
+        <Grid container spacing={2}>
+          {isLoading && <Loader />}
+          {isError && (
+            <Alert severity="error">Could not fetch any assets!</Alert>
+          )}
+          {assets.map((asset) => (
+            <Grid key={asset.id} xs={6} md={4} item>
+              <AssetCard asset={asset} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
   );
 }
 
